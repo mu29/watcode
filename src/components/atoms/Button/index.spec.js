f1ecc49c50d578f991037bfa4eeeca4cda2da4c9
InJung Chung <mu29@yeoubi.net>
2018-05-12 16:02:23 +0900

    스냅샷 테스트를 추가한다

--- src/components/atoms/Button/index.spec.js
+++ src/components/atoms/Button/index.spec.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import 'jest-styled-components'
 import { basicTheme } from 'components/themes'
 import Button from '.'
 
@@ -9,9 +10,14 @@ const wrap = (props = {}) =>
   shallow(<Button theme={ basicTheme } onClick={ onClick } { ...props } />)
 
 describe('<Button />', () => {
+  it('정상적으로 렌더링된다', () => {
+    const wrapper = wrap()
+    expect(wrapper).toMatchSnapshot()
+  })
+
   it('자식 컴포넌트가 들어온 경우 그려준다', () => {
     const wrapper = wrap({ children: 'test' })
-    expect(wrapper.contains('test')).toBe(true)
+    expect(wrapper).toMatchSnapshot()
   })
 
   it('임의의 속성이 들어온 경우 그려준다', () => {

95b5fc60857a9d310cbd0df0c79f2d177aa4a876
InJung Chung <mu29@yeoubi.net>
2018-05-13 17:52:07 +0900

    ArtworkList 컴포넌트와 컨테이너를 추가한다

--- src/components/organisms/index.js
+++ src/components/organisms/index.js
@@ -1,3 +1,4 @@
+export { default as ArtworkList } from './ArtworkList'
 export { default as Header } from './Header'
 export { default as LoginForm } from './LoginForm'
 export { default as ReduxField } from './ReduxField'

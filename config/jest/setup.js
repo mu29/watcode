dc407cdef83b5592bd98fa3a8f3fdfda613dad07
InJung Chung <mu29@yeoubi.net>
2018-05-19 14:26:29 +0900

    테스트 환경을 수정한다

--- config/jest/setup.js
+++ config/jest/setup.js
@@ -1,4 +1,21 @@
-import { configure } from 'enzyme'
+import React from 'react'
+import { mount, render, shallow, configure } from 'enzyme'
+import { Provider } from 'react-redux'
 import Adapter from 'enzyme-adapter-react-16'
+import configureStore from 'store/configure'
+import { basicTheme } from 'components/themes'
 
 configure({ adapter: new Adapter() })
+
+global.mount = mount
+global.render = render
+global.shallow = shallow
+
+global.withTheme = component => React.cloneElement(component, { theme: basicTheme })
+
+const store = configureStore({})
+global.withStore = component => (
+  <Provider store={ store }>
+    { React.cloneElement(component) }
+  </Provider>
+)

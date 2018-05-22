d63642cbf8d2877ded24079221ea4890c2a9d675
InJung Chung <mu29@yeoubi.net>
2018-05-22 14:49:10 +0900

    셀렉터 내보내는 방법을 변경한다

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -3,7 +3,9 @@ import * as entity from './entity/selectors'
 import * as loading from './loading/selectors'
 import * as post from './post/selectors'
 
-module.exports[auth] = auth
-module.exports[entity] = entity
-module.exports[loading] = loading
-module.exports[post] = post
+const selectors = [auth, entity, loading, post]
+  .reduce((a, c) => ({ ...a, ...c }), {})
+
+Object.keys(selectors).forEach((name) => {
+  module.exports[name] = selectors[name]
+})

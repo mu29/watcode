d66a57dde0ec1da65ef05430ea89bddf25ed4d07
InJung Chung <mu29@yeoubi.net>
2018-06-16 00:05:12 +0900

    LoginForm, RegisterForm 컴포넌트를 삭제한다

--- src/components/organisms/LoginForm/index.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import { LoginForm } from 'containers'
-
-storiesOf('Organisms/LoginForm', module)
-  .add('기본', () => (
-    <LoginForm />
-  ))
--- src/components/organisms/RegisterForm/index.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import { RegisterForm } from 'containers'
-
-storiesOf('Organisms/RegisterForm', module)
-  .add('기본', () => (
-    <RegisterForm />
-  ))

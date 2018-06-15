c2c3092b8055fb868b8b8147714931512d0e1bcf
InJung Chung <mu29@yeoubi.net>
2018-06-15 23:46:56 +0900

    테스트를 위한 mocking이 제대로 작동하지 않던 오류를 수정한다

--- config/jest/setup.js
+++ config/jest/setup.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { mount, render, shallow, configure } from 'enzyme'
+import { Provider } from 'react-redux'
 import Adapter from 'enzyme-adapter-react-16'
+import configureStore from 'store/configure'
 import { basicTheme } from 'components/themes'
 
 configure({ adapter: new Adapter() })
@@ -10,3 +12,10 @@ global.render = render
 global.shallow = shallow
 
 global.withTheme = component => React.cloneElement(component, { theme: basicTheme })
+
+const store = configureStore({})
+global.withStore = component => (
+  <Provider store={ store }>
+    { React.cloneElement(component) }
+  </Provider>
+)

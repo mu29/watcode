159bcd7b20ada9f6f4128ade81c2dd3bf589d28b
InJung Chung <mu29@yeoubi.net>
2018-06-15 21:52:15 +0900

    action, component, reducer, saga, selector mock을 추가한다

--- config/jest/setup.js
+++ config/jest/setup.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import { mount, render, shallow, configure } from 'enzyme'
-import { Provider } from 'react-redux'
 import Adapter from 'enzyme-adapter-react-16'
-import configureStore from 'store/configure'
 import { basicTheme } from 'components/themes'
 
 configure({ adapter: new Adapter() })
@@ -12,10 +10,3 @@ global.render = render
 global.shallow = shallow
 
 global.withTheme = component => React.cloneElement(component, { theme: basicTheme })
-
-const store = configureStore({})
-global.withStore = component => (
-  <Provider store={ store }>
-    { React.cloneElement(component) }
-  </Provider>
-)

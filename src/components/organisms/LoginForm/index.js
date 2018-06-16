e55b92cd9d57b96df435cf3fa1a37ec4917e4f19
Lee ji hyung <jihyung@ssm.kr>
2018-06-16 16:43:20 +0900

    LoginForm 컴포넌트를 추가한다

---
+++ src/components/organisms/LoginForm/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { Field } from 'redux-form'
+import { Button, ReduxField } from 'components'
+import { palette } from 'services/style'
+
+const Form = styled.form`
+  margin: auto;
+  padding: 1rem;
+  border: 0.0625rem solid ${palette('gray.30')};
+`
+
+const StyledReduxField = styled(props => <ReduxField { ...props } />)`
+  margin-top: 1rem;
+
+  &:first-child {
+    margin-top: 0;
+  }
+`
+
+const StyledButton = styled(Button)`
+  margin-top: 1.5rem;
+`
+
+const LoginForm = ({
+  handleSubmit, isLoading,
+}) => (
+  <Form onSubmit={ handleSubmit }>
+    <Field label="사용자명" name="name" placeholder="로그인 혹은 회원가입을 위한 사용자 이름입니다." component={ StyledReduxField } />
+    <Field label="비밀번호" name="password" placeholder="4자 이상의 비밀번호를 입력해주세요." component={ StyledReduxField } />
+    <StyledButton type="submit" disabled={ isLoading } block>시작하기</StyledButton>
+  </Form>
+)
+
+LoginForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+}
+
+export default LoginForm

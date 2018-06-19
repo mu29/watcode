0c835466628540464ac0e34695d455d0519bb351
InJung Chung <mu29@yeoubi.net>
2018-06-20 01:24:21 +0900

    Tag 컴포넌트에 Type 속성을 추가한다

--- src/components/atoms/Tag/index.js
+++ src/components/atoms/Tag/index.js
@@ -1,16 +1,21 @@
 import styled from 'styled-components'
-import { palette } from 'services/style'
+import { fonts, palette } from 'services/style'
+
+const fontWeight = ({ type }) => (type ? 400 : 200)
+const color = ({ type, theme }) => palette(type ? 'white.default' : 'gray.90')({ theme })
+const backgroundColor = ({ type, theme }) => palette(type ? 'gray.100' : 'gray.30')({ theme })
 
 const Tag = styled.div`
   display: inline-block;
-  margin-right: 0.375rem;
-  padding: 0.25rem 0.5rem;
-  font-size: 0.875rem;
-  font-weight: 200;
-  color: ${palette('gray.60')};
-  background-color: ${palette('gray.20')};
+  margin-right: 0.25rem;
+  margin: 0.1875rem 0.375rem 0.1875rem 0;
+  padding: 0.125rem 0.375rem;
+  font-family: ${fonts('default')};
+  font-size: 0.75rem;
+  font-weight: ${fontWeight};
+  color: ${color};
+  background-color: ${backgroundColor};
   border-radius: 0.25rem;
-  white-space: nowrap;
 `
 
 export default Tag

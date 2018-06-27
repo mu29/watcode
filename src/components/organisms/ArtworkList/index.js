a189ce238ae6574e7b30b077b2204fc83b7f0364
InJung Chung <mu29@yeoubi.net>
2018-06-27 11:21:41 +0900

    ArtworkList 컴포넌트에서 Wrapper 를 제거한다

--- src/components/organisms/ArtworkList/index.js
+++ src/components/organisms/ArtworkList/index.js
@@ -1,15 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
 import { ArtworkItem } from 'components'
 
-const Wrapper = styled.div``
-
-const ArtworkList = ({ list }) => (
-  <Wrapper>
-    { list.map(item => (<ArtworkItem key={ item.code } artwork={ item } />)) }
-  </Wrapper>
-)
+const ArtworkList = ({ list }) => list.map(item => (
+  <ArtworkItem key={ item.code } artwork={ item } />
+))
 
 ArtworkList.propTypes = {
   list: PropTypes.arrayOf(PropTypes.shape({})).isRequired,

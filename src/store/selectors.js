95e9266c98a1ec2f20073d530fa7bb7922d65883
InJung Chung <mu29@yeoubi.net>
2018-07-06 20:52:35 +0900

    selector를 제대로 불러오지 못하던 오류를 수정한다

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,19 +1,9 @@
-import upperFirst from 'lodash/upperFirst'
-
 const req = require.context('.', true, /\.\/.+\/selectors\.js$/)
 
 req.keys().forEach((key) => {
-  const storeName = key.replace(/\.\/(.+)\/.+$/, '$1')
-  const fromName = `from${upperFirst(storeName)}`
-  const selectors = req(key)
-  const getState = (state = {}) => state[storeName] || {}
+  const actions = req(key)
 
-  module.exports[fromName] = {}
-
-  Object.keys(selectors).forEach((name) => {
-    const selector = selectors[name]
-    if (typeof selector === 'function') {
-      module.exports[fromName][name] = (state, ...args) => selector(getState(state), ...args)
-    }
+  Object.keys(actions).forEach((name) => {
+    module.exports[name] = actions[name]
   })
 })

1492cd0f10fcfb8117729de8f66107521230dc64
InJung Chung <mu29@yeoubi.net>
2018-07-06 21:28:14 +0900

    회원가입, 로그인 폼 내용이 제대로 채워졌는지 확인한다

--- src/containers/SignInForm.js
+++ src/containers/SignInForm.js
@@ -5,19 +5,26 @@ import { reduxForm } from 'redux-form'
 import { SignInForm } from 'components'
 import { signInActions } from 'store/actions'
 import { getIsLoading } from 'store/selectors'
+import { createValidator, required, email, minLength } from 'services/validation'
 
 const SignInFormContainer = props => <SignInForm { ...props } />
+
+const validate = createValidator({
+  email: [required, email],
+  password: [required, minLength(6)],
+})
+
+const onSubmit = (data, dispatch) => dispatch(signInActions.request(data))
 
 const mapStateToProps = state => ({
   isLoading: getIsLoading(state, signInActions.type),
 })
-
-const onSubmit = (data, dispatch) => dispatch(signInActions.request(data))
 
 export default compose(
   connect(mapStateToProps),
   reduxForm({
     form: 'signIn',
     onSubmit,
+    validate,
   }),
 )(SignInFormContainer)
--- src/containers/SignUpForm.js
+++ src/containers/SignUpForm.js
@@ -5,19 +5,27 @@ import { reduxForm } from 'redux-form'
 import { SignUpForm } from 'components'
 import { signUpActions } from 'store/actions'
 import { getIsLoading } from 'store/selectors'
+import { createValidator, required, email, minLength } from 'services/validation'
 
 const SignUpFormContainer = props => <SignUpForm { ...props } />
+
+const validate = createValidator({
+  email: [required, email],
+  name: [required, minLength(1)],
+  password: [required, minLength(6)],
+})
+
+const onSubmit = (data, dispatch) => dispatch(signUpActions.request(data))
 
 const mapStateToProps = state => ({
   isLoading: getIsLoading(state, signUpActions.type),
 })
-
-const onSubmit = (data, dispatch) => dispatch(signUpActions.request(data))
 
 export default compose(
   connect(mapStateToProps),
   reduxForm({
     form: 'signUp',
     onSubmit,
+    validate,
   }),
 )(SignUpFormContainer)

9e433d6d4b335ad94911f4ac2da884913211efa2
InJung Chung <mu29@yeoubi.net>
2018-07-06 20:53:15 +0900

    회원가입, 로그인 컨테이너에 액션과 데이터를 연결한다

--- src/containers/SignInForm.js
+++ src/containers/SignInForm.js
@@ -3,15 +3,16 @@ import { compose } from 'recompose'
 import { connect } from 'react-redux'
 import { reduxForm } from 'redux-form'
 import { SignInForm } from 'components'
+import { signInActions } from 'store/actions'
 import { getIsLoading } from 'store/selectors'
 
 const SignInFormContainer = props => <SignInForm { ...props } />
 
-const onSubmit = (data, dispatch) => {}
-
 const mapStateToProps = state => ({
-  isLoading: false,
+  isLoading: getIsLoading(state, signInActions.type),
 })
+
+const onSubmit = (data, dispatch) => dispatch(signInActions.request(data))
 
 export default compose(
   connect(mapStateToProps),
--- src/containers/SignUpForm.js
+++ src/containers/SignUpForm.js
@@ -3,15 +3,16 @@ import { compose } from 'recompose'
 import { connect } from 'react-redux'
 import { reduxForm } from 'redux-form'
 import { SignUpForm } from 'components'
+import { signUpActions } from 'store/actions'
 import { getIsLoading } from 'store/selectors'
 
 const SignUpFormContainer = props => <SignUpForm { ...props } />
 
-const onSubmit = (data, dispatch) => {}
-
 const mapStateToProps = state => ({
-  isLoading: false,
+  isLoading: getIsLoading(state, signUpActions.type),
 })
+
+const onSubmit = (data, dispatch) => dispatch(signUpActions.request(data))
 
 export default compose(
   connect(mapStateToProps),

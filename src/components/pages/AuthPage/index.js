b74d53592ce2863f0119d6e3df8f3c1ecf385669
InJung Chung <mu29@yeoubi.net>
2018-07-09 16:25:27 +0900

    AuthForm 컴포넌트를 추가한다

--- src/components/pages/AuthPage/index.js
+++ src/components/pages/AuthPage/index.js
@@ -1,30 +1,9 @@
 import React from 'react'
-import styled from 'styled-components'
-import { Tab, TabPage, Header, GenericTemplate } from 'components'
-import { SignInForm, SignUpForm } from 'containers'
-import { palette } from 'services/style'
-
-const StyledTab = styled(Tab)`
-  margin: 1rem 0;
-  border: 0.0625rem solid ${palette('gray.40')};
-  background-color: ${palette('white.default')};
-
-  @media(max-width: 768px) {
-    margin: 0;
-    border: none;
-  }
-`
+import { Header, AuthForm, GenericTemplate } from 'components'
 
 const AuthPage = () => (
   <GenericTemplate header={ <Header location="/auth" /> }>
-    <StyledTab>
-      <TabPage name="로그인">
-        <SignInForm />
-      </TabPage>
-      <TabPage name="회원가입">
-        <SignUpForm />
-      </TabPage>
-    </StyledTab>
+    <AuthForm />
   </GenericTemplate>
 )
 

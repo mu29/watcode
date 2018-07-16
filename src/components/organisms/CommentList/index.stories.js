f5596d4c91c851b0550bffcf840fb587acb7d3a3
InJung Chung <mu29@yeoubi.net>
2018-07-17 01:33:28 +0900

    서버에서 댓글 목록을 불러와 표시한다

--- src/components/organisms/CommentList/index.stories.js
+++ src/components/organisms/CommentList/index.stories.js
@@ -4,22 +4,16 @@ import CommentList from '.'
 
 const comments = [{
   id: 1,
+  author: '영화번역가 황석희',
   content: '달인급 캐릭터 분량 조절의 기술. 이 긴 영화에 지루함 한 톨 없다. 너는 어른이 다 됐더라, 피터',
-  userAttributes: {
-    name: '영화번역가 황석희',
-  },
 }, {
   id: 2,
+  author: '이동진',
   content: '마블의 모든 것을 쏟아부은 명작',
-  userAttributes: {
-    name: '이동진',
-  },
 }, {
   id: 3,
+  author: '뮤',
   content: '선발대 확인완료 돌격하라!!',
-  userAttributes: {
-    name: '뮤',
-  },
 }]
 
 storiesOf('Organisms/CommentList', module)
--- src/containers/CommentList.js
+++ src/containers/CommentList.js
@@ -1,29 +1,21 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { CommentList } from 'components'
+import { withPaginate } from 'wrappers'
+import { readCommentsActions } from 'store/actions'
+import { getArtworkComments, getIsLoading } from 'store/selectors'
 
-const CommentListContainer = props => <CommentList { ...props } />
+const PaginableCommentList = withPaginate()(props => <CommentList { ...props } />)
 
-const mapStateToProps = () => ({
-  list: [{
-    id: 1,
-    content: '마블의 모든 것을 쏟아부은 명작',
-    userAttributes: {
-      name: '이동진',
-    },
-  }, {
-    id: 2,
-    content: '달인급 캐릭터 분량 조절의 기술. 이 긴 영화에 지루함 한 톨 없다. 너는 어른이 다 됐더라, 피터',
-    userAttributes: {
-      name: '영화번역가 황석희',
-    },
-  }, {
-    id: 3,
-    content: '선발대 확인완료 돌격하라!!',
-    userAttributes: {
-      name: '뮤',
-    },
-  }],
+const CommentListContainer = props => <PaginableCommentList { ...props } />
+
+const mapStateToProps = (state, props) => ({
+  list: getArtworkComments(state, props),
+  isLoading: getIsLoading(state, readCommentsActions.type),
 })
 
-export default connect(mapStateToProps)(CommentListContainer)
+const mapDispatchToProps = (dispatch, { id }) => ({
+  onPaginate: () => dispatch(readCommentsActions.request({ id })),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CommentListContainer)

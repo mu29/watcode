808f1e5723b2a5ba252d2c09fa9e15ae44f9e219
InJung Chung <mu29@yeoubi.net>
2018-07-18 18:46:53 +0900

    SearchArtworkList 컨테이너를 추가한다

---
+++ src/containers/SearchArtworkList.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { ArtworkList } from 'components'
+import { withPaginate } from 'wrappers'
+import { searchArtworksActions } from 'store/actions'
+import { getSearchArtworks, getIsLoading } from 'store/selectors'
+
+const PaginableArtworkList = withPaginate()(props => <ArtworkList { ...props } />)
+
+const ArtworkListContainer = props => <PaginableArtworkList { ...props } />
+
+const mapStateToProps = (state, props) => ({
+  list: getSearchArtworks(state, props),
+  cursor: state.artwork.search[props.type].cursor,
+  isLoading: getIsLoading(state, searchArtworksActions.type, 'type', props.type),
+})
+
+const mapDispatchToProps = (dispatch, { type, query }) => ({
+  onPaginate: cursor => dispatch(searchArtworksActions.request({ type, query, cursor })),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(ArtworkListContainer)

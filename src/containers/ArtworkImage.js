8e302a9a1fa4b3910764542684ab0edf6beaea20
InJung Chung <mu29@yeoubi.net>
2018-07-18 15:25:32 +0900

    이미 즐겨찾기 한 작품을 다시 누를 경우 즐겨찾기 해제한다

--- src/containers/ArtworkImage.js
+++ src/containers/ArtworkImage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { ArtworkImage } from 'components'
-import { createBookmarkActions } from 'store/actions'
+import { createBookmarkActions, deleteBookmarkActions } from 'store/actions'
 import { getIsLoading } from 'store/selectors'
 
 const ArtworkImageContainer = props => <ArtworkImage { ...props } />
@@ -9,11 +9,14 @@ const ArtworkImageContainer = props => <ArtworkImage { ...props } />
 const mapStateToProps = (state, { id }) => ({
   email: state.auth.user.email,
   isBookmarked: state.bookmark.bookmarks.find(b => b === id) !== undefined,
-  isLoading: getIsLoading(state, createBookmarkActions.type),
+  isLoading: getIsLoading(state, createBookmarkActions.type)
+    || getIsLoading(state, deleteBookmarkActions.type),
 })
 
-const mapDispatchToProps = (dispatch, { id, email }) => ({
-  toggleBookmark: () => dispatch(createBookmarkActions.request({ id, email })),
+const mapDispatchToProps = (dispatch, { id, email, isBookmarked }) => ({
+  toggleBookmark: () => dispatch(isBookmarked ?
+    deleteBookmarkActions.request({ id }) :
+    createBookmarkActions.request({ id, email })),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(ArtworkImageContainer)

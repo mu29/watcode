6f4176efc3fb7537c7a07efb9ead3dd5e0d6f11a
InJung Chung <mu29@yeoubi.net>
2018-07-19 13:40:43 +0900

    즐겨찾기 로딩 인디케이터를 표시한다

--- src/containers/BookmarkList.js
+++ src/containers/BookmarkList.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { ArtworkList } from 'components'
-import { getBookmarks } from 'store/selectors'
+import { withPaginate } from 'wrappers'
+import { readBookmarksActions } from 'store/actions'
+import { getBookmarks, getIsLoading } from 'store/selectors'
 
-const BookmarkListContainer = props => <ArtworkList { ...props } />
+const PaginableArtworkList = withPaginate()(props => <ArtworkList { ...props } />)
+
+const BookmarkListContainer = props => <PaginableArtworkList { ...props } />
 
 const mapStateToProps = state => ({
   list: getBookmarks(state),
+  isLoading: getIsLoading(state, readBookmarksActions.type),
+  onPaginate: () => {},
 })
 
 export default connect(mapStateToProps)(BookmarkListContainer)

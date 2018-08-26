2891dc581e874fd397fd10131122eb46f6b2bd70
InJung Chung <mu29@yeoubi.net>
2018-08-26 12:03:37 +0900

    즐겨찾기 일괄 생성 액션을 추가한다

--- src/store/bookmark/actions.js
+++ src/store/bookmark/actions.js
@@ -4,6 +4,7 @@ import { artworkSchema } from '../schemas'
 const factory = actionCreatorFactory('Bookmark')
 
 export const createBookmarkActions = factory.async('CREATE_BOOKMARK', { schema: artworkSchema })
+export const createBookmarksActions = factory.async('CREATE_BOOKMARKS', { schema: artworkSchema })
 export const readBookmarksActions = factory.async('READ_BOOKMARKS', { schema: artworkSchema })
 export const deleteBookmarkActions = factory.async('DELETE_BOOKMARK')
 export const readRecommendationsActions = factory.async('READ_RECOMMENDATIONS', { schema: artworkSchema })
